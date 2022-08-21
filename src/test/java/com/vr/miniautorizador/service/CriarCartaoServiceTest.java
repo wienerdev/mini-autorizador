6c410ff050a87bc3e685348ed7a71cea95a3797b
wienerdev <matheuswiener9@gmail.com>
2022-08-21 15:27:50 -0300

    JUnit encorporado ao projeto

---
+++ src/test/java/com/vr/miniautorizador/service/CriarCartaoServiceTest.java
@@ -0,0 +1,21 @@
+package com.vr.miniautorizador.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CriarCartaoServiceTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void testSimples() {
+        
+
+    }
+
+
+    
+}
